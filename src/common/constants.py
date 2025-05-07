5623864429b56003dc8f9a2379b2cca1db025e08
WORKSPACE <yurii.oksamytnyi@yuriodev.co.uk>
2025-05-07 12:33:10 +0100

    Update constants.py

--- src/common/constants.py
+++ src/common/constants.py
@@ -1,10 +1,20 @@
 # Multimodal Transfer Learning / Image Captioning
 # File: src/common/constants.py
 # Copyright (c) 2025 Dropout Disco Team (Yurii, Artemis, Nnamdi, Kenton)
-# Description: Shared constants (PAD_ID, START_ID, etc.).
-# Created: 2025-05-05
-# Updated: 2025-05-05
+# Description: Defines common constants like special token IDs.
+# Created: 2025-05-06
+# Updated: 2025-05-06
 
-# Basic pass statement or main block for runnable scripts
-if __name__ == "__main__":
-    pass
+PAD_TOKEN = "[PAD]"
+START_TOKEN = "[START]" # Or [SOS], [BOS]
+END_TOKEN = "[END]"   # Or [EOS]
+UNK_TOKEN = "[UNK]"
+
+# Placeholder IDs - these will be updated by the tokenizer
+PAD_TOKEN_ID = 0
+START_TOKEN_ID = 1
+END_TOKEN_ID = 2
+UNK_TOKEN_ID = 3
+
+# Other constants can be added here as needed
+# E.g., DEFAULT_MAX_SEQ_LEN = 50

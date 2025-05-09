2f652f05faf160504408e93cab96ac639ec14aa3
WORKSPACE <yurii.oksamytnyi@yuriodev.co.uk>
2025-05-09 14:34:33 +0100

    Create feature_datasets.py

---
+++ src/data/feature_datasets.py
@@ -0,0 +1,10 @@
+# Multimodal Transfer Learning / Image Captioning
+# File: src/data/feature_datasets.py
+# Copyright (c) 2025 Dropout Disco Team (Yurii, Artemis, Nnamdi, Kenton)
+# Description: (Optional) Dataset classes loading pre-computed image features.
+# Created: 2025-05-05
+# Updated: 2025-05-05
+
+# Basic pass statement or main block for runnable scripts
+if __name__ == "__main__":
+    pass
